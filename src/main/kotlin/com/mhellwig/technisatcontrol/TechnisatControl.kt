f012162448b0254de642da1655cc4ddac5c2a7b5
Martin Hellwig <martinhellwig@gmx.de>
2023-12-27 22:14:04 +0100

    Close socket after each request

--- src/main/kotlin/com/mhellwig/technisatcontrol/TechnisatControl.kt
+++ src/main/kotlin/com/mhellwig/technisatcontrol/TechnisatControl.kt
@@ -20,18 +20,16 @@ class TechnisatControl(private val ipAddress: String) {
             .bind(InetSocketAddress("0.0.0.0", 8090))
         val receiverAddress = InetSocketAddress(ipAddress, 8090)
 
-        try {
+        socket.use { autoCloseSocket ->
             controlEnums.forEach {
                 var sendData = ("<rcuButtonRequest code=\"${it.technicalValue}\" state=\"pressed\" />").toByteArray()
                 var sendPacket = Datagram(ByteReadPacket(sendData), receiverAddress)
-                socket.send(sendPacket)
+                autoCloseSocket.send(sendPacket)
 
                 sendData = "<rcuButtonRequest code=\"${it.technicalValue}\" state=\"released\" />".toByteArray()
                 sendPacket = Datagram(ByteReadPacket(sendData), receiverAddress)
-                socket.send(sendPacket)
+                autoCloseSocket.send(sendPacket)
             }
-        } catch (e: Throwable) {
-            socket.close()
         }
     }
 
